35853ea86d8269a82861c76644f1f60bef04d244
Almerino Buce <AlmerinoBuce998@gmail.com>
2022-10-20 11:22:26 +0200

    added dynamic routes for posts and stylesheet

--- apollo-client.js
+++ apollo-client.js
@@ -1,9 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    // uri: "https://countries.trevorblades.com",
-    uri: "http://localhost/wordpress_nextjs/graphql",
-    // uri: "https://127.0.0.1:8080/graphql",
+    uri: `${process.env.NEXT_PUBLIC_API_URL}`,
     cache: new InMemoryCache(),
 });
 
